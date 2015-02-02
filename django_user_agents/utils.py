f13c2288c15e898f9a2a87c0b51aac19923eede3
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2015-02-02 08:09:58 -0200

    When request have no user_agent, instance with blank string

--- django_user_agents/utils.py
+++ django_user_agents/utils.py
@@ -24,7 +24,7 @@ def get_user_agent(request):
     # Tries to get UserAgent objects from cache before constructing a UserAgent
     # from scratch because parsing regexes.yaml/json (ua-parser) is slow
     if not hasattr(request, 'META'):
-        return None
+        return ''
 
     ua_string = request.META.get('HTTP_USER_AGENT', '')
     key = get_cache_key(ua_string)
@@ -40,6 +40,9 @@ def get_and_set_user_agent(request):
     # call get_user_agent and attach it to request so it can be reused
     if hasattr(request, 'user_agent'):
         return request.user_agent
+
+    if not request:
+        return parse('')
 
     request.user_agent = get_user_agent(request)
     return request.user_agent
