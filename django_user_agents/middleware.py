1ec9d3b5d7a2fdfd6e7d0e763c95e1a3117cd96d
Greg Schafer <grschafer@gmail.com>
2016-08-08 19:11:56 +0000

    Update middleware to be django1.10-compatible

--- django_user_agents/middleware.py
+++ django_user_agents/middleware.py
@@ -1,9 +1,10 @@
 from django.utils.functional import SimpleLazyObject
+from django.utils.deprecation import MiddlewareMixin
 
 from .utils import get_user_agent
 
 
-class UserAgentMiddleware(object):
+class UserAgentMiddleware(MiddlewareMixin):
     # A middleware that adds a "user_agent" object to request
     def process_request(self, request):
         request.user_agent = SimpleLazyObject(lambda: get_user_agent(request))
