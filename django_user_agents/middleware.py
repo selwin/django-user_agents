cbd6774bbf630cf8d3328e7d5dce401b59582211
Danny Boscan <dannyboscan@gmail.com>
2016-11-17 21:30:15 -0300

    middleware for new and older version of django

--- django_user_agents/middleware.py
+++ django_user_agents/middleware.py
@@ -1,10 +1,17 @@
 from django.utils.functional import SimpleLazyObject
-from django.utils.deprecation import MiddlewareMixin
 
 from .utils import get_user_agent
 
 
-class UserAgentMiddleware(MiddlewareMixin):
-    # A middleware that adds a "user_agent" object to request
+class CustomUserAgentMiddleware(object):
+
+    def __init__(self, get_response=None):
+        if get_response is not None:
+            self.get_response = get_response
+
+    def __call__(self, request):
+        self.process_request(request)
+        return self.get_response(request)
+
     def process_request(self, request):
         request.user_agent = SimpleLazyObject(lambda: get_user_agent(request))
